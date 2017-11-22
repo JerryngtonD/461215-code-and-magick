8121545e05496a0e9660bfab6d28d96f5e8534dc
jerryngton <jerryngtond@gmail.com>
2017-11-22 21:47:39 +0300

    Пофиксил ошибки по codestyle

--- js/stat.js
+++ js/stat.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-trailing-spaces,no-multiple-empty-lines,no-console */
 // stats.js
-
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+
+  var defaultIndent = 0;
+  var histogramWidth = 150;
+
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(110, 20, 420, 270);
@@ -14,52 +17,53 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
-  ctx.shadowColor = 'transparent'
+  ctx.shadowColor = 'transparent';
 
   ctx.fillStyle = '#000';
   ctx.font = '14px PT Mono';
 
-  ctx.fillText('Ура вы победили!', 120, 40);
+  var initializeXtext = 120;
+  var initializeYtext = 40;
+  ctx.fillText('Ура вы победили!', initializeXtext, initializeYtext);
 
   var max = -1;
-  var maxIndex = -1;
-
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
-      maxIndex = i;
     }
   }
-
-  var histogramWidth = 150;
-  var step = histogramWidth / (max - 0);
-
-  ctx.fillText('Список результатов', 120, 60);
-
   
+  var step = histogramWidth / (max - defaultIndent);
+
+  var initializeYresult = 60;
+  ctx.fillText('Список результатов', initializeXtext, initializeYresult);
+
+
   function sortKeys(object) {
-    var keysSorted = Object.keys(object).sort(function (a, b) {return object[a] - object[b]});
+    var keysSorted = Object.keys(object).sort(function (a, b) {
+      return object[a] - object[b];
+    });
     return keysSorted;
   }
 
   function getObjNamesAndValues() {
     var NamestoTimes = {};
-    for (var i = 0; i < names.length; i++) {
-      NamestoTimes['' + names[i]] = times[i];
+    for (var index = 0; index < names.length; index++) {
+      NamestoTimes['' + names[index]] = times[index];
     }
     return NamestoTimes;
   }
 
   function getSortedObj(object) {
     var sortedKeys = sortKeys(object);
-    var sortedValues = times.sort(function(a, b) {
+    var sortedValues = times.sort(function (a, b) {
       return b - a;
     });
 
     var NamestoTimes = {};
-    for (var i = 0; i < sortedKeys.length; i++) {
-      NamestoTimes['' + sortedKeys[i]] = sortedValues[i];
+    for (var index = 0; index < sortedKeys.length; index++) {
+      NamestoTimes['' + sortedKeys[index]] = sortedValues[index];
     }
     return NamestoTimes;
   }
@@ -69,25 +73,22 @@ window.renderStatistics = function (ctx, names, times) {
 
   var counter = 0;
   for (var propt in namesAndTimesObj) {
-    if (propt == "Вы") {
-      ctx.fillStyle = "rgba(255, 0, 0, " + 1 + ")";
-    }
-    else {
+    if (propt === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, ' + 1 + ')';
+    } else {
       var opacity = 0;
-      while (opacity == 0) {
+      while (opacity === 0) {
         opacity = Math.random();
       }
-      console.log(opacity)
-      ctx.fillStyle = "rgba(0, 0, 255, " + opacity + ")";
+      console.log(opacity);
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + opacity + ')';
     }
     ctx.fillRect(140 + 80 * counter, 250, 40, -namesAndTimesObj[propt] * step);
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = '#000000';
     ctx.fillText(propt, 140 + 80 * counter, 270);
     ctx.fillText(Math.round(namesAndTimesObj[propt]), 140 + 80 * counter, 240 - namesAndTimesObj[propt] * step);
 
     counter++;
-
-
   }
 
 };
